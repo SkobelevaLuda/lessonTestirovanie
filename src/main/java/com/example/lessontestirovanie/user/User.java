bd0b874b3ae96c6af429c85c03e4fef0c5cf626d
Людок <skobeleva.ludmila@yandex.ru>
2023-02-02 12:42:40 +0300

    Перввый коммит

--- src/main/java/com/example/lessontestirovanie/user/User.java
+++ src/main/java/com/example/lessontestirovanie/user/User.java
@@ -1,2 +1,35 @@
-package com.example.lessontestirovanie.user;public class User {
+package com.example.lessontestirovanie.user;
+
+import java.util.Objects;
+
+public class User {
+
+    private final String login;
+    private final String email;
+
+    public User(String login, String email) {
+        this.login = login;
+        this.email = email;
+    }
+
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User user)) return false;
+        return Objects.equals(login, user.login) && Objects.equals(email, user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(login, email);
+    }
 }
--- src/test/java/com/example/lessontestirovanie/tests/UserTest.java
+++ src/test/java/com/example/lessontestirovanie/tests/UserTest.java
@@ -1,2 +1,20 @@
-package com.example.lessontestirovanie.tests;public class UserTest {
+package com.example.lessontestirovanie.tests;
+
+import com.example.lessontestirovanie.user.User;
+import org.junit.Test;
+
+public class UserTest {
+
+    @Test
+    public void getUser() {
+
+        User user = new User("Tanya123", "tanya@123ya.ru");
+    }
+    @Test
+    public void getUserNOParametrs() {
+
+        User user = new User("Tanya123", "tanya@123ya.ru");
+    }
+
+
 }
